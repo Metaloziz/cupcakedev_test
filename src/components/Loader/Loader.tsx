b915f01c7031e85c4a2ebd0d244ce8e1b3532703
Andrew Gaityukevich <AndrewGaity@yandex.by>
2023-04-19 14:41:51 +0300

    fix: Loader rerender

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import style from './Loader.module.css'
 import React from "react";
 
-export const Loader: FC = () => {
-  return (
+export const Loader: FC = memo(() => {
+    return (
     <div className={style.loader}/>
   );
-};
+});
