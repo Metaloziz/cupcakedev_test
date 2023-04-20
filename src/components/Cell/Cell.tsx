8095c85e471e238c114ba4d194d29310513b779d
Andrew Gaityukevich <AndrewGaity@yandex.by>
2023-04-20 09:46:38 +0300

    feat: don't need React import

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from 'react';
+import { FC, memo } from 'react';
 
 import { CellPropsT } from '../../types/components/CellPropsT';
 import { rounderThreeDigits } from '../../utils/rounderThreeDigits';
